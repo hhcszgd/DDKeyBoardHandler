be3a18991355abe07d86c6093ddd8b6c0f8a058d
wangyuanfei <979433528@qq.com>
2018-05-14 14:46:22 +0800

    optimize logic

--- KeyBoardHandler/DDKeyBoardHandler.swift
+++ KeyBoardHandler/DDKeyBoardHandler.swift
@@ -23,38 +23,54 @@ class DDKeyBoardHandler: NSObject {
     /// - Parameters:
     ///   - containerView: inputView's container , it will be move while keyboard hide or show
     ///   - inPutView: inPutView
+    /// for example :
+    /*
+     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+     DDKeyBoardHandler.share.setViewToBeDealt(containerView: getCashContainer, inPutView: textField)
+     return true
+     }
+     */
     func setViewToBeDealt(containerView:UIView , inPutView : UIView) {
-        self.containerView = containerView
+        if inPutView.frame == CGRect.zero {
+            self.containerView = nil
+            self.inPutView = nil
+            originalFrame = CGRect.zero
+            return
+        }
         self.inPutView = inPutView
-        originalFrame = containerView.frame
+        guard  let  tempContainerView = self.containerView , tempContainerView == containerView else{
+            self.containerView = containerView
+            originalFrame = containerView.frame
+            return
+        }
     }
     private func getInputViewFrameInWindow()  -> CGRect{
         if let window = UIApplication.shared.delegate?.window  , let input = self.inPutView{
-           let rect =  input.convert(input.bounds, to: window)
+            let rect =  input.convert(input.bounds, to: window)
             return rect
         }
         return CGRect.zero
     }
     private func prepare()  {
-            NotificationCenter.default.addObserver(forName: .UIKeyboardWillShow, object: nil  , queue: OperationQueue.main) { (notification) in
-                if self.containerView != nil {
-                    let keyboardFrame = self.getKeyboardFrameFromNotification(notification: notification)
-                    let viewFrame = self.getInputViewFrameInWindow()
-                    if viewFrame.maxY > keyboardFrame.minY {
-                        UIView.animate(withDuration: 0.25, animations: {
-                            let cha = viewFrame.maxY - keyboardFrame.minY
-                            self.containerView!.frame.origin.y -= cha
-                        })
-                    }
-                }
-            }
-            NotificationCenter.default.addObserver(forName: .UIKeyboardWillHide, object: nil  , queue: OperationQueue.main) { (notification) in
-                if self.containerView != nil {
+        NotificationCenter.default.addObserver(forName: .UIKeyboardWillShow, object: nil  , queue: OperationQueue.main) { (notification) in
+            if self.containerView != nil {
+                let keyboardFrame = self.getKeyboardFrameFromNotification(notification: notification)
+                let viewFrame = self.getInputViewFrameInWindow()
+                if viewFrame.maxY > keyboardFrame.minY {
                     UIView.animate(withDuration: 0.25, animations: {
-                        self.containerView!.frame = self.originalFrame
+                        let cha = viewFrame.maxY - keyboardFrame.minY
+                        self.containerView!.frame.origin.y -= cha
                     })
                 }
             }
+        }
+        NotificationCenter.default.addObserver(forName: .UIKeyboardWillHide, object: nil  , queue: OperationQueue.main) { (notification) in
+            if self.containerView != nil {
+                UIView.animate(withDuration: 0.25, animations: {
+                    self.containerView!.frame = self.originalFrame
+                })
+            }
+        }
     }
     
     private func getKeyboardFrameFromNotification(notification : Notification) -> CGRect {
@@ -65,3 +81,4 @@ class DDKeyBoardHandler: NSObject {
     }
     
 }
+
